cc5c40e7cbf07df8760160b88f880b9ab96596e9
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-18 16:41:46 -0400

    Cria método de busca de todos os usuários em UserController

--- src/main/java/com/macedo/api_recovery_games/controller/UserController.java
+++ src/main/java/com/macedo/api_recovery_games/controller/UserController.java
@@ -6,6 +6,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,6 +26,11 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
         return ResponseEntity.ok(service.getUserById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(service.getAllUsers());
     }
 
 }
