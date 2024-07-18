3ca95fc69ff059641f73868b35fb8707a1915fd0
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-18 16:09:54 -0400

    Cria metodo para salvar usuário

--- src/main/java/com/macedo/api_recovery_games/service/UserService.java
+++ src/main/java/com/macedo/api_recovery_games/service/UserService.java
@@ -1,7 +1,27 @@
 package com.macedo.api_recovery_games.service;
 
+import com.macedo.api_recovery_games.models.User;
+import com.macedo.api_recovery_games.models.dtos.UserDTO;
+import com.macedo.api_recovery_games.models.mapper.UserMapper;
+import com.macedo.api_recovery_games.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository repository;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepository repository, UserMapper userMapper) {
+        this.repository = repository;
+        this.userMapper = userMapper;
+    }
+
+    @Transactional
+    public UserDTO saveUser(UserDTO userDTO) {
+        User user = userMapper.toEntity(userDTO);
+        repository.save(user);
+        return userMapper.toDTO(user);
+    }
 }
