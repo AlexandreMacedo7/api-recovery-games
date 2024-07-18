01bb7823039bf607fbd4983f1ec169ffbe8b9675
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-18 16:10:23 -0400

    Cria metodo para receber usuario em UserController

--- src/main/java/com/macedo/api_recovery_games/controller/UserController.java
+++ src/main/java/com/macedo/api_recovery_games/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.macedo.api_recovery_games.controller;
 
+import com.macedo.api_recovery_games.models.dtos.UserDTO;
+import com.macedo.api_recovery_games.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService service;
 
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(service.saveUser(userDTO));
+    }
 
 }
