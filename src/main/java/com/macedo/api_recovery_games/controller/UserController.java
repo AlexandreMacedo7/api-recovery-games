2ae620f0719e26bb7546bd84ff5e70f522139f8d
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-18 16:04:36 -0400

    Cria classe UserController

---
+++ src/main/java/com/macedo/api_recovery_games/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.macedo.api_recovery_games.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+
+}
