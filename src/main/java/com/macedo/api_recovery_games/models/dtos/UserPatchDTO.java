a524355d54de4f0c05a446988e8bdd5e3fec14a5
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-20 16:31:58 -0400

    Cria record de UserPatchDTO para receber atualizações de campos individuais

---
+++ src/main/java/com/macedo/api_recovery_games/models/dtos/UserPatchDTO.java
@@ -0,0 +1,11 @@
+package com.macedo.api_recovery_games.models.dtos;
+
+import java.util.Optional;
+
+public record UserPatchDTO(Optional<String> name, Optional<String> phone) {
+
+    public UserPatchDTO {
+        name = name != null ? name : Optional.empty();
+        phone = phone != null ? phone : Optional.empty();
+    }
+}
