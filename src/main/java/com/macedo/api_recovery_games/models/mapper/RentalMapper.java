48b42bd33587a7851c7befde8b17fed8ba78a603
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-20 20:51:03 -0400

    Cria RentalMapper

---
+++ src/main/java/com/macedo/api_recovery_games/models/mapper/RentalMapper.java
@@ -0,0 +1,26 @@
+package com.macedo.api_recovery_games.models.mapper;
+
+import com.macedo.api_recovery_games.models.Rental;
+import com.macedo.api_recovery_games.models.dtos.RentalDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RentalMapper {
+
+    @Mapping(source = "machine.id", target = "machineId")
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "totalCost", target = "totalCost")
+    RentalDTO toDTO(Rental rental);
+
+    @Mapping(source = "machineId", target = "machine.id")
+    @Mapping(source = "userId", target = "user.id")
+    @Mapping(source = "totalCost", target = "totalCost")
+    Rental toEntity(RentalDTO rentalDTO);
+
+    List<RentalDTO> toDTOList(List<Rental> rentalList);
+
+    List<Rental> toEntituList(List<RentalDTO> rentalDTOS);
+}
