c0d684723a93d28ff02c7ddf4f15ea61eee7ab73
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-20 21:35:51 -0400

    Cria método para criar Rental

--- src/main/java/com/macedo/api_recovery_games/controller/RentalController.java
+++ src/main/java/com/macedo/api_recovery_games/controller/RentalController.java
@@ -1,5 +1,11 @@
 package com.macedo.api_recovery_games.controller;
 
+import com.macedo.api_recovery_games.models.dtos.RentalDTO;
+import com.macedo.api_recovery_games.service.RentalService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("rentals")
 public class RentalController {
 
+    private final RentalService rentalService;
+
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+
+    @PostMapping
+    public ResponseEntity<RentalDTO> createRental(@RequestBody @Valid RentalDTO rentalDTO) {
+        return ResponseEntity.ok(rentalService.saveRental(rentalDTO));
+    }
 }
