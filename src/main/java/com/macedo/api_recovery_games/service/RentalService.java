675708aab6e27d8e59cfd72d88192667a0812c74
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-21 21:21:33 -0400

    Cria método patch para atualização de Rental

--- src/main/java/com/macedo/api_recovery_games/service/RentalService.java
+++ src/main/java/com/macedo/api_recovery_games/service/RentalService.java
@@ -3,12 +3,14 @@ package com.macedo.api_recovery_games.service;
 import com.macedo.api_recovery_games.exception.RentalNotFoundException;
 import com.macedo.api_recovery_games.models.Rental;
 import com.macedo.api_recovery_games.models.dtos.RentalDTO;
+import com.macedo.api_recovery_games.models.dtos.RentalPatchDTO;
 import com.macedo.api_recovery_games.models.mapper.RentalMapper;
 import com.macedo.api_recovery_games.repository.RentalRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RentalService {
@@ -37,6 +39,12 @@ public class RentalService {
         return mapper.toDTO(rental);
     }
 
+    @Transactional
+    public RentalDTO patchRental(Long id, RentalPatchDTO patchDTO) {
+        Optional<Rental> rentalOptional = Optional.ofNullable(repository.findById(id).orElseThrow(() -> new RentalNotFoundException(id)));
+        return mapper.toDTO(updateFields(patchDTO, rentalOptional));
+    }
+
     public RentalDTO getRentalById(Long id) {
         Rental rental = repository.findById(id).orElseThrow(() -> new RentalNotFoundException(id));
         return mapper.toDTO(rental);
@@ -52,6 +60,17 @@ public class RentalService {
         } else {
             throw new RentalNotFoundException(id);
         }
+    }
+
+    private Rental updateFields(RentalPatchDTO patchDTO, Optional<Rental> rentalOptional) {
+        Rental rental = rentalOptional.get();
+
+        patchDTO.startTime().ifPresent(rental::setStartTime);
+        patchDTO.endTime().ifPresent(rental::setEndTime);
+        patchDTO.totalCost().ifPresent(rental::setTotalCost);
+
+        repository.save(rental);
+        return rental;
     }
 
     private void validateMachineAndUser(Long idMachine, Long idUser) {
