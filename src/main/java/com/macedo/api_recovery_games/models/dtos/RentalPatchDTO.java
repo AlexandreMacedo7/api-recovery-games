e979f88ed8c2ffe468c8c422701d9008b56bf01d
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-21 21:18:37 -0400

    Cria record RentalPatchDTO

---
+++ src/main/java/com/macedo/api_recovery_games/models/dtos/RentalPatchDTO.java
@@ -0,0 +1,16 @@
+package com.macedo.api_recovery_games.models.dtos;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public record RentalPatchDTO(Optional<LocalDateTime> startTime,
+                             Optional<LocalDateTime> endTime,
+                             Optional<BigDecimal> totalCost) {
+
+    public RentalPatchDTO {
+        startTime = startTime != null ? startTime : Optional.empty();
+        endTime = endTime != null ? endTime : Optional.empty();
+        totalCost = totalCost != null ? totalCost : Optional.empty();
+    }
+}
