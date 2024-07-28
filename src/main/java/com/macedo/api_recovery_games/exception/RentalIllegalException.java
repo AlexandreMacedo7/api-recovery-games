49aa8efbf273b484c7121694299c7471e0b625a2
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-28 16:25:22 -0400

    Cria exceção generica para Rental

---
+++ src/main/java/com/macedo/api_recovery_games/exception/RentalIllegalException.java
@@ -0,0 +1,7 @@
+package com.macedo.api_recovery_games.exception;
+
+public class RentalIllegalException extends RuntimeException{
+    public RentalIllegalException(String message) {
+        super(message);
+    }
+}
