978b460c2fa7eb69cd396d4c451ceacf93c76680
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-07-28 16:23:16 -0400

    Remove os parametros de starTime e endTime do RentalDTO

--- src/main/java/com/macedo/api_recovery_games/models/dtos/RentalDTO.java
+++ src/main/java/com/macedo/api_recovery_games/models/dtos/RentalDTO.java
@@ -3,9 +3,7 @@ package com.macedo.api_recovery_games.models.dtos;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public record RentalDTO(@NotNull Long machineId, @NotNull Long userId,
-                        @NotNull LocalDateTime startTime, @NotNull LocalDateTime endTime,
                         @NotNull BigDecimal totalCost) {
 }
