56bd039b7826c5410ec94dcc42a2223086fcc331
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-08-30 16:34:26 -0400

    Cria ControlResponseDTO

---
+++ src/main/java/com/macedo/api_recovery_games/models/dtos/ControlResponseDTO.java
@@ -0,0 +1,4 @@
+package com.macedo.api_recovery_games.models.dtos;
+
+public record ControlResponseDTO(Long id, Integer number, Double valueControl) {
+}
--- src/main/java/com/macedo/api_recovery_games/models/mapper/ControlMapper.java
+++ src/main/java/com/macedo/api_recovery_games/models/mapper/ControlMapper.java
@@ -2,6 +2,7 @@ package com.macedo.api_recovery_games.models.mapper;
 
 import com.macedo.api_recovery_games.models.Control;
 import com.macedo.api_recovery_games.models.dtos.ControlDTO;
+import com.macedo.api_recovery_games.models.dtos.ControlResponseDTO;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -9,11 +10,11 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface ControlMapper {
 
-    ControlDTO toDTO(Control control);
+    ControlResponseDTO toDTO(Control control);
 
     Control toEntity(ControlDTO controlDTO);
 
-    List<ControlDTO> toDTOList(List<Control> controlsList);
+    List<ControlResponseDTO> toDTOList(List<Control> controlsList);
 
     List<Control> toEntityList(List<ControlDTO> controlDTOList);
 
