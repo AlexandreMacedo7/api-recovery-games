0333f3d1eafa6991a4d1ef2443de3987aa13f643
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-09-03 11:15:46 -0400

    Renomeia metóto de mapper de control para retorno com detalhes em dto

--- src/main/java/com/macedo/api_recovery_games/models/mapper/ControlMapper.java
+++ src/main/java/com/macedo/api_recovery_games/models/mapper/ControlMapper.java
@@ -3,6 +3,7 @@ package com.macedo.api_recovery_games.models.mapper;
 import com.macedo.api_recovery_games.models.Control;
 import com.macedo.api_recovery_games.models.dtos.CreateControlDTO;
 import com.macedo.api_recovery_games.models.dtos.ControlResponseDTO;
+import com.macedo.api_recovery_games.models.dtos.DetailsControlDTO;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -15,6 +16,8 @@ public interface ControlMapper {
     Control toEntity(CreateControlDTO createControlDTO);
 
     List<ControlResponseDTO> toDTOList(List<Control> controlsList);
+
+    List<DetailsControlDTO> toDetailsDTOList(List<Control> controlsList);
 
     List<Control> toEntityList(List<CreateControlDTO> createControlDTOList);
 
